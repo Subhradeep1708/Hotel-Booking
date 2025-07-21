84b0f29745c4839b0c5c0962802aca8142329e1a
Subhradeep1708 <subhradeep1708@gmail.com>
2025-07-21 16:41:49 +0530

    Add actual frontend files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      HomePageasdsada\\asd0-9asdpo
+      askdjsoaidhj
+      askjashdashdg
+    </div>
+  )
+}
+
+export default Home
